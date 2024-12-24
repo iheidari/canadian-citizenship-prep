9b93e2f9e94bcbdb460bc1bdcb92ba36b7451469
Iman Heidari <iman.heydari@gmail.com>
2024-12-23 16:51:13 -0800

    fix the build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./ui/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
