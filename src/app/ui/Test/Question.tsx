682544caba2a9b31d13d7a16e8636775a73b4ad3
Iman Heidari <iman.heydari@gmail.com>
2024-12-24 16:33:47 -0800

    small change for small display

--- src/app/ui/Test/Question.tsx
+++ src/app/ui/Test/Question.tsx
@@ -15,15 +15,15 @@ const Question = (props: CarouselProps) => {
           <div className="grid h-full justify-center text-center w-full">
             <div className="grid min-h-[450px] w-full md:w-[600px] grid-rows-[min-content_minmax(0,1fr)] gap-6 transform translate-z-0 overflow-visible px-4">
               {/* Question */}
-              <h2 className="text-lg md:text-2xl font-bold mt-6 text-left md:text-center md:whitespace-normal">
+              <h2 className="text-base sm:text-lg md:text-2xl font-bold mt-4 md:mt-6 text-left md:text-center md:whitespace-normal">
                 {props.question.question}
               </h2>
               {/* Options */}
-              <div className="mb-8">
+              <div className="mb-6 md:mb-8">
                 {props.question.options.map((option, index) => (
                   <label
                     key={index}
-                    className={`block py-4 cursor-pointer text-base md:text-lg ${
+                    className={`block py-2 md:py-4 cursor-pointer text-base md:text-lg ${
                       props.selectedOption === index
                         ? "text-blue-400"
                         : "text-white"
